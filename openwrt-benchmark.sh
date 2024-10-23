fd052ef7bd7f6314b2255c67cdbeeae56665f08c
Hideo Suzumiya <yhyxwgy@gmail.com>
2024-10-24 00:45:40 +0800

    Fix compatibility check for OpenWrt in benchmark script

--- openwrt-benchmark.sh
+++ openwrt-benchmark.sh
@@ -2,7 +2,7 @@
 
 #set -x
 
-NAME=$(grep NAME /etc/os-release | head -n 1 | awk -F '"' '{print $2}')
+ID_LIKE=$(grep ID_LIKE /etc/os-release | head -n 1 | awk -F '"' '{print $2}')
 
 NETNS="wg-bench"
 NIC="wg-bench"
@@ -13,7 +13,7 @@ NS_WG_IP="169.254.200.2"
 HOST_PORT="11001"
 NS_PORT="11002"
 
-if [ "$NAME" != "OpenWrt" ]; then
+if [ "$ID_LIKE" != "lede openwrt" ]; then
     echo "This is not OpenWrt. Exit"
     exit 1
 fi
